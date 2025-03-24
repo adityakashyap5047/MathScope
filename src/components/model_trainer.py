a74d70fb3acb574e000e8a0c9f410765f91c6e4d
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-25 01:30:11 +0530

    starting wiht model training part

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -17,3 +17,26 @@ from sklearn.metrics import r2_score
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_object
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+        try:
+            logging.info("Spliting training and test data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, : -1],
+                train_array[:, -1],
+                test_array[:, : -1],
+                test_array[:, -1]
+            )
+
+            
+
+        except:
+            pass

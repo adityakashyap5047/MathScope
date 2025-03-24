bd834d68b165b37df29efcd39bd074a7786a8958
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-25 00:41:06 +0530

    saving the pickle file in the directory

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
+from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
@@ -18,7 +19,7 @@ class DataTransformationConfig:
 
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config = DataTransformation()
+        self.data_transformation_config = DataTransformationConfig()
 
     def get_data_transformation_object(self):
         '''
@@ -97,6 +98,17 @@ class DataTransformation:
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
             logging.info(f"Saved preprocessing object.")
+
+            save_object(
+                file_path = self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessing_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
 
         except:
 

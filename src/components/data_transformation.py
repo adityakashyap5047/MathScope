0e4c82ff738eb9ac12783588f4fb383055666c4b
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-25 00:27:28 +0530

    Applying preprocessing object on training dataframe and testing dataframe.

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -66,3 +66,30 @@ class DataTransformation:
         except Exception as e:
 
             raise CustomException(e, sys)
+        
+    def initiate_data_transformation(self, train_path, test_path):
+
+        try:
+            train_df = pd.read_csv  (train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Read train and test data completed")
+
+            logging.info("obtaining preprocessing object")
+
+            preprocessing_obj = self.get_data_transformation_object()
+
+            target_column_name = "math_score"
+            numerical_columns = ["writing_score", "reading_score"]
+
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = test_df[target_column_name]
+
+            logging.info(f"Applying preprocessing object on training dataframe and testing dataframe.")
+
+        except:
+
+            pass

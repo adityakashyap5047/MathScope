0873de9ccb90f043e3d345ce73c0353abded3e04
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-25 00:13:10 +0530

    Completed the pipeline for numeical and categorical columns

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from dataclasses import dataclass
 
 import numpy as np
@@ -11,3 +12,45 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from src.exception import CustomException
 from src.logger import logging
 
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts', "preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformation()
+
+    def get_data_transformation_object(self):
+        try:
+            numerical_columns = ["writing_score", "reading_score"],
+            categorical_columns = [
+                "gender",
+                "race_ethnicity",
+                "parential_level_of_education",
+                "lunch",
+                "test_preparation_course"
+            ]
+
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", StandardScaler)
+                ]
+            )
+
+            cat_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder", OneHotEncoder()),
+                    ("scaler", StandardScaler())
+                ]
+            )
+
+            logging.info("Numerical columns standard scaling completed")
+
+            logging.info("Categorical columns encoding completed")
+
+            
+
+        except:
+            pass

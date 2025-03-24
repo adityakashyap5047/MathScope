72d8ae889a048b637595001e1ead26333d46c1d9
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-25 02:29:01 +0530

    complete the model training part

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -26,7 +26,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
 
-    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Spliting training and test data")
             X_train, y_train, X_test, y_test = (
@@ -49,8 +49,23 @@ class ModelTrainer:
 
             model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
 
+            best_model_score = max(sorted(model_report.values()))
 
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
             
+            best_model = models[best_model_name]
 
-        except:
-            pass
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
+
+            logging.info(f"Best model found")
+
+            save_object(self.model_trainer_config.trained_model_file_path, best_model)
+
+            return r2_score(y_test, best_model.predict(X_test))
+
+        except Exception as e:
+            
+            raise CustomException(e, sys)

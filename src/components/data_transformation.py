444950ba9af9568b3a8c9b70128c00c52dbc882c
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-25 00:31:34 +0530

    Saved preprocessing object

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -86,9 +86,17 @@ class DataTransformation:
             target_feature_train_df = train_df[target_column_name]
 
             input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
-            target_feature_train_df = test_df[target_column_name]
+            target_feature_test_df = test_df[target_column_name]
 
             logging.info(f"Applying preprocessing object on training dataframe and testing dataframe.")
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info(f"Saved preprocessing object.")
 
         except:
 
